97cbed0c2b3f1eca0058dd15dc3925a1824f0085
man93 <manjulyadav93@gmail.com>
2016-10-01 17:50:31 +0530

    Update diff_tab_with_diff_ops.sh

--- diff_tab_with_diff_ops.sh
+++ diff_tab_with_diff_ops.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#this is used to open multiple tabs in single window with performing different operations 
 gnome-terminal --tab -e "bash -c '{ cd /home/manjul/programs; vi test.cpp; }';bash" --tab -e "bash -c 'cal';bash" --tab -e "bash -c 'date';bash" --tab -e "bash -c 'who';bash" --tab -e "bash -c 'ping 127.0.0.1';bash" --tab -e "bash -c 'netstat';bash" --tab -e "bash -c '{ date; cal; }';bash"
