049f20609791ab14b527e378e84a63aec498948e
man93 <manjulyadav93@gmail.com>
2016-10-03 15:09:06 +0530

    Update email_sender_script_python.py

--- email_sender_script_python.py
+++ email_sender_script_python.py
@@ -1,7 +1,16 @@
 import smtplib	
+#connection to server
+#for Gmail--smtp.gmail.com
+#for yahoo--smtp.mail.yahoo.com
+#for outlook--smtp-mail.outlook.com
 smtpobj=smtplib.SMTP('smtp.gmail.com', 587)
+#first hello packet
 smtpobj.ehlo()
+#for encription
 smtpobj.starttls()
-smtpobj.login('manjulyadav93@gmail.com', '!@#$%^&*()_+')
+#your login to your mail account
+smtpobj.login('YOUR_EMAIL_ADDRESS', 'YOUR_PASSWORD')
+#from address,to address ,Subject:something.\n Now your mail body msg
 smtpobj.sendmail('manjulyadav93@gmail.com', 'manjulyadav93@gmail.com', 'Subject:Trail mail from python.\nHi Manjul,How are you?Have you got your salary')
+#disconnection
 smtpobj.quit()
