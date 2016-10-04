eb3000bd6ae9428299b87ead75daa042d2110970
man93 <manjulyadav93@gmail.com>
2016-10-04 16:11:50 +0530

    Update create_docx_with_python.py

--- create_docx_with_python.py
+++ create_docx_with_python.py
@@ -1,7 +1,11 @@
 import docx
+#create document object
 doc=docx.Document()
+#adding paragraph to document
 doc.add_paragraph('Hello World!')
 paraobj1=doc.add_paragraph('This is second paragraph.')
 paraobj2=doc.add_paragraph('This is third paragraph.')
-paraobj1.add_run('This text i am adding at the end of the first paragrapg.')
+#adding this line to end of first paragraph. 
+paraobj1.add_run('This text i am adding at the end of the first paragraph.')
+#saving the document
 doc.save('multipleparagraphs.docx')
