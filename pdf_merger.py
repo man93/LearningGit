0697f6907936309b39087a1ba4b6106bd275c8b2
man93 <manjulyadav93@gmail.com>
2016-10-04 16:15:32 +0530

    Update pdf_merger.py

--- pdf_merger.py
+++ pdf_merger.py
@@ -1,8 +1,8 @@
 import PyPDF2
-pdffile1=open('mozilla.pdf', 'rb')
-pdffile2=open('Manjul_Amazon.pdf', 'rb')
-pdfreader1=PyPDF2.PdfFileReader(pdffile1)
-pdfreader2=PyPDF2.PdfFileReader(pdffile2)
+pdf1=open('mozilla.pdf', 'rb')
+pdf2=open('Manjul_Amazon.pdf', 'rb')
+pdfreader1=PyPDF2.PdfFileReader(pdf1)
+pdfreader2=PyPDF2.PdfFileReader(pdf2)
 pdfwriter=PyPDF2.PdfFileWriter()
 for i in range(pdfreader1.numPages):
      page1=pdfreader1.getPage(i)
@@ -13,5 +13,5 @@ for j in range(pdfreader2.numPages):
 pdfout=open('mergedpdf.pdf', 'wb')
 pdfwriter.write(pdfout)
 pdfout.close()
-pdffile1.close()
-pdffile2.close()
+pdf1.close()
+pdf2.close()
