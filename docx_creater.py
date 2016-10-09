295fbc9f32814b383a64bdd053764ff7b46c8d8a
Manjul Yadav <manjulyadav93@gmail.com>
2016-10-09 17:02:57 +0530

    Delete docx_creater.py

--- docx_creater.py
+++
@@ -1,7 +0,0 @@
-import docx
-doc=docx.Document()
-doc.add_paragraph('Hello World!')
-paraobj1=doc.add_paragraph('This is second paragraph.')
-paraobj2=doc.add_paragraph('This is third paragraph.')
-paraobj1.add_run('This text i am adding at the end of the first paragrapg.')
-doc.save('multipleparagraphs.docx')
